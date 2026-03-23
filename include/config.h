e24d5fac8bb600fe11fb9f28e1ddff478196b61e
jypipi <jeffzc@umich.edu>
2026-03-22 21:53:41 -0400

    modified files for running

--- include/config.h
+++ include/config.h
@@ -7,9 +7,14 @@
 
 #define STR(x) #x //for stringizing
 
+//cmake source dir will be written here
 // Windows path:
 // #define CMAKE_SOURCE_DIR STR(/mnt/c/Users/Jeffrey Chen/Desktop/NL-2-Actions/ReloPush)
+
 // Docker path:
-#define CMAKE_SOURCE_DIR STR(/ws/NL-2-Actions/ReloPush) //cmake source dir will be written here
+// #define CMAKE_SOURCE_DIR STR(/ws/NL-2-Actions/ReloPush)
+
+// Lab Desktop path (Linux 20.04):
+#define CMAKE_SOURCE_DIR STR(/home/jeffrey/NL-2-Actions/ReloPush)
 
 #endif // CONFIG_H_IN
